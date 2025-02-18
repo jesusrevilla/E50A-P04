cc51d40478c4d5c4fc808e62c2595a05263a2b7b
vulletz <122696042+vulletz@users.noreply.github.com>
2025-02-17 18:38:32 -0600

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,22 +1,27 @@
+\echo 'Sebastian Heredia Pardo - 175680\n\n'
 -- create table clientes (id, email)
-
-
-
-
-
+CREATE TABLE clientes (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(100) NOT NULL UNIQUE
+);
 
 -- create table facturas (id, client_id)
-
-
-
-
-
+CREATE TABLE facturas (
+  id SERIAL PRIMARY KEY,
+  client_id INTEGER,
+  FOREIGN KEY (client_id) REFERENCES clientes(id)
+);
 
 -- create table productos (id, nombre, precio)
-
-
-
-
-
+CREATE TABLE productos (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  precio NUMERIC CHECK (precio > 10)
+);
 
 -- create table envios (id, fecha, estado)
+CREATE TABLE envios (
+  id SERIAL PRIMARY KEY,
+  fecha DATE,
+  estado VARCHAR(100) CHECK (estado IN ('preparando', 'en tránsito', 'entregado'))
+);
