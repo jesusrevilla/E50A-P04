76abb6d72bdd3cbfb194b6655b70628ebf94403f
170973 <170973@upslp.edu.mx>
2025-02-18 18:24:54 -0600

    fatures/170973.sql

--- scripts.sql
+++ scripts.sql
@@ -1,5 +1,29 @@
 -- create table clientes (id, email)
 
+CREATE TABLE clientes (
+  Id_c SERIAL PRIMARY KEY,
+  email VARCHAR(30) UNIQUE NOT NULL
+  
+);
+CREATE TABLE facturas (
+  Id_f SERIAL PRIMARY KEY,
+  cliente_Id INT REFERENCES clientes(Id_c) NOT NULL
+  
+);
+
+CREATE TABLE productos (
+  Id_p SERIAL PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  precio NUMERIC CHECK (precio> 10)
+);
+
+CREATE TABLE envios(
+  Id_e SERIAL PRIMARY KEY,
+  Estado VARCHAR(20) CHECK (Estado IN ('preparando', 'en tránsito', 'entregado')) 
+
+);
+
+
 
 
 
